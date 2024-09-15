49e926c054d251419be56ad96e47038137a4adf5
sebastiansilvaaa <134339912+sebastiansilvaaa@users.noreply.github.com>
2024-09-15 11:35:33 -0300

    Delete escenario1/2_particiones directory

--- escenario1/2_particiones/api/grpc_client.py
+++
@@ -1,9 +0,0 @@
-import grpc
-import cache_pb2
-import cache_pb2_grpc
-
-def get_dns_resolution(domain):
-    with grpc.insecure_channel('grpc_server:50051') as channel:
-        stub = cache_pb2_grpc.DNSResolverStub(channel)
-        response = stub.Resolve(cache_pb2.ResolveRequest(domain=domain))
-    return response.ip
