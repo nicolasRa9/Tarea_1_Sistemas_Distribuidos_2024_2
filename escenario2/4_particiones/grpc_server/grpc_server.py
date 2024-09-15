9ea5ee6c042387a67ce15cfdf7fe58dd44f1b8aa
nicolasRa9 <148264258+nicolasRa9@users.noreply.github.com>
2024-09-15 12:28:56 -0300

    Update grpc_server.py

--- escenario2/4_particiones/grpc_server/grpc_server.py
+++ escenario2/4_particiones/grpc_server/grpc_server.py
@@ -8,7 +8,9 @@ import hashlib
 # Conexión a Redis
 redis_clients = [
     redis.StrictRedis(host='redis1', port=6379, db=0),
-    redis.StrictRedis(host='redis2', port=6379, db=0)
+    redis.StrictRedis(host='redis2', port=6379, db=0),
+    redis.StrictRedis(host='redis3', port=6379, db=0),
+    redis.StrictRedis(host='redis4', port=6379, db=0)
 ]
 
 def get_redis_instance(domain):
